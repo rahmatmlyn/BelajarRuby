37262285246dcf5748792897fa052d8ac90db148
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-04 08:30:47 +0700

    Lanjutan belajar

--- materioop2.rb
+++ materioop2.rb
@@ -2,3 +2,7 @@
 # adalah sebuah metode pewarisan dalam bahasa indonesia 
 
 # Contohnya ketika kita mempunyai class terbaru dan ingin mengambil method dari class tertentu kita tidak perlu menulis ulang code dari class lain cukup dengan mewarisi class nya 
+
+class 
+    
+end
