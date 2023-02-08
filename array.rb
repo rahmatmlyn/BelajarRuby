1bc17dcc4b14b703e16269acd34eedf67f4aae83
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-08 23:44:03 +0700

    array Ruby

---
+++ array.rb
@@ -0,0 +1,16 @@
+# array ruby sama kayak array pada umummnya untuk penulisannya namun berbda pada pengambilan datanya
+#  TIPE DATA ARRAY DI RUBY TIDAK DIMULAI DARI 0 MELAINKAN 1 
+
+angka = [1, 2, 3 ]
+bahasa = ["ruby", "C#", "html", "css"]
+
+puts "Ambil data pertama : " + bahasa.first
+puts "Ambil data terakhir : " + bahasa.last
+
+#drop
+puts "Jumlah Array : #{bahasa.count}"
+puts "length Array : #{bahasa.length}"
+puts "Ambil data 3 : #{bahasa.take(0)}"
+puts "Ambil data 3 : #{bahasa.take(2)}"
+
+
