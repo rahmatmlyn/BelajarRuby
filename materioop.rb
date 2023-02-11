a3b5887aa159b1387fcc7b9679c030ae9f10f067
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-11 22:46:13 +0700

    OOP Ruby

---
+++ materioop.rb
@@ -0,0 +1,9 @@
+# OOP merupakan singkatan dari Object Oriented Programming
+
+# class merupakan sebuah kontainer yang berguna untuk membungkus semua kode yang ada di dalam class tersebut
+# keuntungan dari class ini adalah 
+ 
+=begin
+1. DRY atau biasa disebut Don’t Repeat Yourself
+
+=end
