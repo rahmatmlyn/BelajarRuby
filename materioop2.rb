3bf38fce833a2ff004e7cb44883ed5011f4e6ac8
rahmatyana26 <90589127+rahmatyana26@users.noreply.github.com>
2023-02-12 21:51:35 +0700

    OOP Ruby inherit

---
+++ materioop2.rb
@@ -0,0 +1,4 @@
+# OOP Inheritance 
+# adalah sebuah metode pewarisan dalam bahasa indonesia 
+
+# Contohnya ketika kita mempunyai class terbaru dan ingin mengambil method dari class tertentu kita tidak perlu menulis ulang code dari class lain cukup dengan mewarisi class nya 
